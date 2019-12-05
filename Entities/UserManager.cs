5a7e75c5db2b70c1fb59475e65469fa863bf9eac
Przemyslaw Madej <przemyslaw.madej0@gmail.com>
2019-12-05 19:06:57 +0100

    add UserManager

---
+++ Entities/UserManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_jwt_authentication.Entities
+{
+    public class UserManager
+    {
+        private List<User> allUsers = new List<User>
+        {
+            new User
+            {
+                Id=1,
+                Email="sarah.connor@domain.com",
+                Password="John",
+                Role="Employee"
+            },
+            new User
+            {
+                Id=2,
+                Email="glenn.quagmire@domain.com",
+                Password="giggity",
+                Role="Manager"
+            }
+        };
+
+        public User GetUser(string email, string passwrod)
+        {
+            return allUsers.SingleOrDefault(x => x.Email == email && x.Password == passwrod);
+        }
+    }
+}
