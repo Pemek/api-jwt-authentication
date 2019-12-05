b660fd5804f7e1012ef6144b3917cb698dc56a9f
Przemyslaw Madej <przemyslaw.madej0@gmail.com>
2019-12-05 21:39:14 +0100

    add SecuredHelloController, add authentication and authorization middleware

--- Controllers/SecuredHelloController.cs
+++ Controllers/SecuredHelloController.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api_jwt_authentication.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class PublicHelloController : ControllerBase
+    public class SecuredHelloController : ControllerBase
     {
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello to everyone");
+            return Ok("Hello to authenticated");
         }
     }
 }
